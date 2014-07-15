18b5eaf37a9852fa15b92c5f71430b8a61815569
AlexVPopov <alex.val.popov@gmail.com>
2014-07-15 11:23:28 +0300

    Create a revert controller action

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,2 +1,7 @@
 class VersionsController < ApplicationController
+  def revert
+    @version = Version.find(param[:id])
+    @version.reify.save!
+    redirect_to :back, notice: "#{@version.event.capitalize} undone!"
+  end
 end
