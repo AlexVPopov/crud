47f81612f4945d17a30d470411d6db61f7d097e8
AlexVPopov <alex.val.popov@gmail.com>
2014-12-11 21:10:58 +0200

    Make driver model accept nested attributes for driving style

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -6,4 +6,6 @@ class Driver < ActiveRecord::Base
     numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   belongs_to :driving_style
+
+  accepts_nested_attributes_for :driving_style
 end
