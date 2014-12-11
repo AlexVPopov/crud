fb0ef659b22c759a426517f4a32dcf6990bd1f42
AlexVPopov <alex.val.popov@gmail.com>
2014-12-11 18:46:51 +0200

    Create a driver model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141211163520) do
+ActiveRecord::Schema.define(version: 20141211164319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20141211163520) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "brand"
+  end
+
+  create_table "drivers", force: true do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "versions", force: true do |t|
