a56bd07cad38e799016b4f57313f4a06bab9ac9e
Alexander Popov <alex.val.popov@gmail.com>
2015-04-17 18:26:10 +0300

    Add a custom validation for ages attributes in Cars model

--- app/models/car.rb
+++ app/models/car.rb
@@ -7,4 +7,13 @@ class Car < ActiveRecord::Base
   validates :year, numericality: { only_integer: true,
                                    greater_than_or_equal_to: 1894,
                                    less_than_or_equal_to: Time.now.year }
+  validate :valid_ages
+
+  private
+
+    def valid_ages
+      if ages.include? 0
+        errors.add :ages, 'cannot contain a string or zero.'
+      end
+    end
 end
